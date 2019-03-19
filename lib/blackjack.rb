04b517ad3a3e2da1f5a41ca3af787e5efa69040b
Gino Desangles <blushingbox@yahoo.com>
2019-03-19 03:38:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,9 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
+def end_game(num)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
